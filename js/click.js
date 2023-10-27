9995a1f24dc9b204a138ecee94f08f95e5abbe59
zilinkids <137368907+zilinkids@users.noreply.github.com>
2023-10-27 12:16:49 +0800

    click.js

--- js/click.js
+++ js/click.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 
 jQuery(document).ready(function($) {
 	$("body").click(function(e) {
-		var a = new Array("爱搞科技的子霖kids","zilinkids的qqBot","小王fly","XiaowangBot","清瑜","Hope解说","可乐","BadJui","python王","BSOD-SYSTEM","乱敲代码pro","爱搞机的学生","编程","手工","点赞","投币","收藏","加关注");
+		var a = new Array("爱搞科技的子霖kids","zilinkids的qqBot","小王fly","XiaowangBot","清瑜","Hope解说","小乐kids","BadJui","python王","BSOD-SYSTEM","乱敲代码pro","爱搞机的学生","编程","手工","点赞","投币","收藏","加关注");
 		var $i = $("<span/>").text(a[a_idx]);
 		a_idx = (a_idx + 1) % a.length;
 		var x = e.pageX, y = e.pageY;
